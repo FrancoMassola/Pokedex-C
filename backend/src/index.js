bedfa20448a8f12431442c2e638bb44aaba9215e
[FrancoMassola] <frann.massola@gmail.com>
2021-04-14 20:20:08 -0300

    connect to database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
 //require express module
 const express = require('express');
+
 //run express module
 const app = express();
-
+require ('./database');
 app.listen(3000);
 console.log('Server on port', 3000);
