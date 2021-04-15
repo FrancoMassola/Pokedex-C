bae6c6c48996729698302a56902b717c9e09e482
[FrancoMassola] <frann.massola@gmail.com>
2021-04-15 20:19:37 -0300

    configurate routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import components
+import {PokemonslistComponent} from './components/pokemonslist/pokemonslist.component';
+import {SigninComponent} from './components/signin/signin.component';
+import {SignupComponent} from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/pokemonslist',
+    pathMatch: 'full'
+  },
+  //private route
+  {
+    path: 'pokemonslist',
+    component: PokemonslistComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
