7b04cfd66dadfe8c32818960d042f280e62df5f3
[FrancoMassola] <frann.massola@gmail.com>
2021-04-14 21:42:10 -0300

    add singin route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
 const router = Router();
 const PokemonTrainer = require("../models/TrainerUser");
-const jwt = require('jsonwebtoken');
-const config = require('../data.env');
+const jwt = require("jsonwebtoken");
+const config = require("../data.env");
 
 //all api routes
 router.get("/", (req, res) => res.send(""));
@@ -15,9 +15,23 @@ router.post("/singup", async (req, res) => {
   const newUser = new PokemonTrainer({ username, password });
   //save to database
   await newUser.save();
-  //create token to send -- payload, secret, options 
-  const token = jwt.sign({_id: newUser._id}, config.SECRET_KEY );
-  res.status(200).json({token});
+  //create token to send -- payload, secret, options
+  const token = jwt.sign({ _id: newUser._id }, config.SECRET_KEY);
+  res.status(200).json({ token });
+});
+
+//route - login 
+router.post('/singin', async (req,res) =>{
+    const {username, password} = req.body;
+    //If the username exist in db save on const user
+    const user = await PokemonTrainer.findOne({username});
+    //if user dont exist
+    if(!user) return res.status(401).send("The user doesn't exist");
+    if(user.password !== password) return res.status(401).send('Wrong password');
+    const token = jwt.sign({_id: user._id}, config.SECRET_KEY);
+    return res.status(200).json({token});
+    
+
 });
 
 module.exports = router;
