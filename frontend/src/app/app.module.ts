714ea04e5340f3dba71309e07f78ab67868611f5
[FrancoMassola] <frann.massola@gmail.com>
2021-04-15 21:16:35 -0300

    configurate signup service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 //add module to manipulate forms
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+//add module to manipulate services
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,22 +11,15 @@ import { SignupComponent } from './components/signup/signup.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { PokemonslistComponent } from './components/pokemonslist/pokemonslist.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     SigninComponent,
     PokemonslistComponent,
-    
-    
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private URL = 'http://localhost:3000/';
 
-  constructor() { }
+  //Generate class instance
+  constructor(private http: HttpClient) {}
+
+  singUp(user) {
+    //return object observable
+    return this.http.post<any>(`${this.URL}/signup`, user);
+  }
 }
